4428c6a342995dd79bd857c0f38ab1c17cda872f
MarkusCasius <2303380@leedstrinity.ac.uk>
2025-08-10 23:25:20 +0100

    Final Edits and Fixes part 2

--- Assessment2/app/src/androidTest/java/com/example/softwaredevelopmentassessment2/ProfileActivityInstrumentedTest.java
+++ Assessment2/app/src/androidTest/java/com/example/softwaredevelopmentassessment2/ProfileActivityInstrumentedTest.java
@@ -1,30 +1,21 @@
 package com.example.softwaredevelopmentassessment2;
 
-import android.content.Intent;
-
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.example.softwaredevelopmentassessment2.ProfileActivity;
-
-import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
-import androidx.test.espresso.IdlingRegistry;
 import static org.hamcrest.Matchers.*;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 @RunWith(AndroidJUnit4.class)
 public class ProfileActivityInstrumentedTest {
